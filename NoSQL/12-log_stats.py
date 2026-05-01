b378f7920bc2d2e25316c824e07b3216a695e0fd
Qudrat-nfs <13551@holbertonstudents.com>
2026-05-01 17:37:03 +0000

    wsd

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 """
-Script that provides some stats about Nginx logs stored in MongoDB
+Script that provides some stats about Nginx logs stored in MongoDB.
 """
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
+    # Connect to MongoDB server
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
+    # 1. Get total number of logs
     total_logs = nginx_collection.count_documents({})
     print(f"{total_logs} logs")
 
+    # 2. Get number of logs for each HTTP method
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
+    # 3. Get number of logs with method=GET and path=/status
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
