098dee2a0b248e8ac7dc446f496ffb462a6e0879
Qudrat-nfs <13551@holbertonstudents.com>
2026-05-01 17:32:07 +0000

    dvs

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,15 +5,16 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx = client.logs.nginx
+    nginx_collection = client.logs.nginx
 
-    print(f"{nginx.count_documents({})} logs")
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
 
-±
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        count = nginx.count_documents({"method": method})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_count = nginx.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_count} status check")
+    status_check = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
